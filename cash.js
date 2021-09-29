2a0a1dec8aebc55e9cb96b33f8e66077916e1867
normandy14 <normandy14@protonmail.com>
2021-09-29 16:53:03 -0400

    Restore long version because other version caused bugs in some edge cases

--- cash.js
+++ cash.js
@@ -55,23 +55,136 @@ checkCashRegister = (price, cash, cid) => {
   else if (checkInsufficentFunds1(cashRemainder) === true) {
     return {'status' : 'INSUFFICIENT_FUNDS', 'change' :  []}
   }
+  (cidObj); // easier lookup of # of bills in cid
 
  
-  for (let key of Object.keys(currencyGlobal).reverse()) {
-  if (cashRemainder / currencyGlobal[key] > 0) {
-    let amount = parseInt(cashRemainder / currencyGlobal[key])
-    let avaliableCash = cidObj[key] / currencyGlobal[key];
-    if (avaliableCash < amount) {
-      amount = avaliableCash;
+  // divisible by hundred's bill
+  if (cashRemainder / 100 > 0) {
+    let hundredDollar = parseInt(cashRemainder / 100);
+    let avaliableCash = cidObj['ONE HUNDRED'] / 100;
+    if (avaliableCash < hundredDollar) { // hun bill fits in, but not enough bills in register to give full amount ie. 300 % 3 One Hundred bills gives 3 One Hundred bills, but only 2 One Hundred bills avaliable, so simply give 2 One hundred bills, because its as close as we can get to close the negative balance
+      hundredDollar = avaliableCash;
     }
-    let record = [key, amount * currencyGlobal[key]];
-    if (record[1] !== 0) {
-      cashReturn.push(record)
+    let hundredRecord = ['ONE HUNDRED', hundredDollar * 100];
+    (hundredRecord)
+    if (hundredRecord[1] !== 0) {
+      cashReturn.push(hundredRecord)
     }
-    cashRemainder = parseFloat((cashRemainder - (currencyGlobal[key] * amount)).toFixed(2));
+    cashRemainder = parseFloat((cashRemainder - (100 * hundredDollar)).toFixed(2));; // b/c in this if statement, assume at least 1 One hundred bill was added to the cashReturn arr
   }
-}
 
+  // divisible by twenty's bill
+  if (cashRemainder / 20 > 0) {
+    let twentDollar = parseInt(cashRemainder / 20);
+    let avaliableCash = cidObj['TWENTY'] / 20;
+    if (avaliableCash < twentDollar) {
+      twentDollar = avaliableCash;
+    }
+    let twentRecord = ['TWENTY', twentDollar * 20];
+    if (twentRecord[1] !== 0) {
+      cashReturn.push(twentRecord)
+    }
+    cashRemainder = parseFloat((cashRemainder - (20 * twentDollar)).toFixed(2));
+  }
+
+  // divisible by ten's bill
+  if (cashRemainder / 10 > 0) {
+    let tenDollar = parseInt(cashRemainder / 10);
+    let avaliableCash = cidObj['TEN'] / 10;
+    if (avaliableCash < tenDollar) {
+      tenDollar = avaliableCash;
+    }
+    let tenRecord = ['TEN', tenDollar * 10];
+    if (tenRecord[1] !== 0) {
+      cashReturn.push(tenRecord)
+    }
+    cashRemainder = parseFloat((cashRemainder - (10 * tenDollar)).toFixed(2));
+  }
+
+  // divisible by five's bill
+  if (cashRemainder / 5 > 0) {
+    let fiveDollar = parseInt(cashRemainder / 5);
+    let avaliableCash = cidObj['FIVE'] / 5;
+    if (avaliableCash < fiveDollar) {
+      fiveDollar = avaliableCash;
+    }
+    let fiveRecord = ['FIVE', fiveDollar * 5];
+    if (fiveRecord[1] !== 0) {
+      cashReturn.push(fiveRecord)
+    }
+    cashRemainder = parseFloat((cashRemainder - (5 * fiveDollar)).toFixed(2));
+  }
+
+  // divisible by one's bill
+  if (cashRemainder / 1 > 0) {
+    let oneDollar = parseInt(cashRemainder / 1);
+    let avaliableCash = cidObj['ONE'] / 1;
+    if (avaliableCash < oneDollar) {
+      oneDollar = avaliableCash;
+    }
+    let oneRecord = ['ONE', oneDollar * 1];
+    if (oneRecord[1] !== 0) {
+      cashReturn.push(oneRecord)
+    }
+    cashRemainder = parseFloat((cashRemainder - (1 * oneDollar)).toFixed(2));
+    (cashRemainder)
+  }
+
+  // divisible by quarter
+  if (cashRemainder / .25 > 0) {
+    let numQuart = parseInt(cashRemainder / .25);
+    let avaliableCash = cidObj['QUARTER'] / .25;
+    if (avaliableCash < numQuart) {
+      numQuart = avaliableCash;
+    }
+    let quarterRecord = ['QUARTER', numQuart * .25];
+    if (quarterRecord[1] !== 0) {
+      cashReturn.push(quarterRecord)
+    }
+    cashRemainder = parseFloat((cashRemainder - (.25 * numQuart)).toFixed(2));
+    (cashRemainder)
+    
+  }
+  // divisible by dime
+  if (cashRemainder / .10 > 0) {
+    let numDime = parseInt(cashRemainder / .10);
+    let avaliableCash = cidObj['DIME'] / .10;
+    if (avaliableCash < numDime) {
+      numDime = avaliableCash;
+    }
+    let dimeRecord = ['DIME', numDime * .10];
+    if (dimeRecord[1] !== 0) {
+      cashReturn.push(dimeRecord)
+    }
+    cashRemainder = parseFloat((cashRemainder - (.10 * numDime)).toFixed(2));
+  }
+
+  // divisible by nickel's
+  if (cashRemainder / .05 > 0) {
+    let numNick = parseInt(cashRemainder / .05);
+    let avaliableCash = cidObj['NICKEL'] / .05;
+    if (avaliableCash < numNick) {
+      numNick = avaliableCash;
+    }
+    let nickRecord = ['NICKEL', numNick * .05];
+    if (nickRecord[1] !== 0) {
+      cashReturn.push(nickRecord)
+    }
+    cashRemainder = parseFloat((cashRemainder - (.05 * numNick)).toFixed(2));
+  }
+  // divisible by pennie's
+  if (cashRemainder / .01 > 0) {
+    let numPenn = parseInt(cashRemainder / .01);
+    let avaliableCash = cidObj['PENNY'] / .01;
+    if (avaliableCash < numPenn) {
+      numPenn = avaliableCash;
+    }
+    let pennRecord = ['PENNY', (numPenn * .01)]; // +.04 account for rounding error
+    if (pennRecord[1] !== 0) {
+      cashReturn.push(pennRecord)
+    }
+    cashRemainder = parseFloat((cashRemainder - (.01 * numPenn)).toFixed(2));
+  }
   // * calc new cashRemainder ie. cashRemainder % 100
 
   // surgecal site
@@ -89,8 +202,7 @@ checkCashRegister = (price, cash, cid) => {
 }
 
 
-
 console.log(
-    checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+    checkCashRegister(17.59, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
   )
   
